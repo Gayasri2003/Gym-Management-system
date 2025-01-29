7ce509bd820d0e809a8fda8c9733568510e090cb
Ravindu Ratnayake <69672524+RavinduRatnayake14@users.noreply.github.com>
2025-01-29 21:30:37 +0530

    Updated Database with Trainer Classes

---
+++ Gym Management System SDAM2/Gym Management System SDAM2/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Gym_Management_System_SDAM2
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginPanel1 = new System.Windows.Forms.Panel();
+            this.closelabel = new System.Windows.Forms.Label();
+            this.loginPanel2 = new System.Windows.Forms.Panel();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.Logolabel = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.showPcheck = new System.Windows.Forms.CheckBox();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.passwdTxt = new System.Windows.Forms.TextBox();
+            this.UsernameTxt = new System.Windows.Forms.TextBox();
+            this.passwdLabel = new System.Windows.Forms.Label();
+            this.UsernameLabel = new System.Windows.Forms.Label();
+            this.WcLabel = new System.Windows.Forms.Label();
+            this.RegisterTxt = new System.Windows.Forms.Label();
+            this.loginPanel1.SuspendLayout();
+            this.loginPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // loginPanel1
+            // 
+            this.loginPanel1.BackColor = System.Drawing.Color.White;
+            this.loginPanel1.Controls.Add(this.RegisterTxt);
+            this.loginPanel1.Controls.Add(this.closelabel);
+            this.loginPanel1.Controls.Add(this.loginPanel2);
+            this.loginPanel1.Controls.Add(this.textBox);
+            this.loginPanel1.Controls.Add(this.showPcheck);
+            this.loginPanel1.Controls.Add(this.loginBtn);
+            this.loginPanel1.Controls.Add(this.passwdTxt);
+            this.loginPanel1.Controls.Add(this.UsernameTxt);
+            this.loginPanel1.Controls.Add(this.passwdLabel);
+            this.loginPanel1.Controls.Add(this.UsernameLabel);
+            this.loginPanel1.Controls.Add(this.WcLabel);
+            this.loginPanel1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.loginPanel1.Location = new System.Drawing.Point(-1, 0);
+            this.loginPanel1.Name = "loginPanel1";
+            this.loginPanel1.Size = new System.Drawing.Size(683, 488);
+            this.loginPanel1.TabIndex = 0;
+            // 
+            // closelabel
+            // 
+            this.closelabel.AutoSize = true;
+            this.closelabel.BackColor = System.Drawing.Color.Red;
+            this.closelabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.closelabel.ForeColor = System.Drawing.Color.White;
+            this.closelabel.Location = new System.Drawing.Point(652, 9);
+            this.closelabel.Name = "closelabel";
+            this.closelabel.Size = new System.Drawing.Size(20, 20);
+            this.closelabel.TabIndex = 11;
+            this.closelabel.Text = "X";
+            this.closelabel.Click += new System.EventHandler(this.closelabel_Click);
+            // 
+            // loginPanel2
+            // 
+            this.loginPanel2.BackColor = System.Drawing.Color.MidnightBlue;
+            this.loginPanel2.Controls.Add(this.pictureBox);
+            this.loginPanel2.Controls.Add(this.Logolabel);
+            this.loginPanel2.Location = new System.Drawing.Point(0, 0);
+            this.loginPanel2.Name = "loginPanel2";
+            this.loginPanel2.Size = new System.Drawing.Size(297, 488);
+            this.loginPanel2.TabIndex = 10;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Image = global::Gym_Management_System_SDAM2.Properties.Resources.icons8_user_100;
+            this.pictureBox.Location = new System.Drawing.Point(85, 149);
+            this.pictureBox.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 12;
+            this.pictureBox.TabStop = false;
+            // 
+            // Logolabel
+            // 
+            this.Logolabel.AutoSize = true;
+            this.Logolabel.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Logolabel.ForeColor = System.Drawing.Color.White;
+            this.Logolabel.Location = new System.Drawing.Point(57, 262);
+            this.Logolabel.Name = "Logolabel";
+            this.Logolabel.Size = new System.Drawing.Size(162, 34);
+            this.Logolabel.TabIndex = 12;
+            this.Logolabel.Text = "Pulse Gym";
+            // 
+            // textBox
+            // 
+            this.textBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox.Location = new System.Drawing.Point(337, 420);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(160, 15);
+            this.textBox.TabIndex = 8;
+            this.textBox.Text = "Don\'t have an account?";
+            this.textBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // showPcheck
+            // 
+            this.showPcheck.AutoSize = true;
+            this.showPcheck.Location = new System.Drawing.Point(513, 275);
+            this.showPcheck.Name = "showPcheck";
+            this.showPcheck.Size = new System.Drawing.Size(125, 20);
+            this.showPcheck.TabIndex = 7;
+            this.showPcheck.Text = "Show Password";
+            this.showPcheck.UseVisualStyleBackColor = true;
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.BackColor = System.Drawing.Color.DarkBlue;
+            this.loginBtn.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginBtn.ForeColor = System.Drawing.Color.White;
+            this.loginBtn.Location = new System.Drawing.Point(337, 316);
+            this.loginBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(91, 44);
+            this.loginBtn.TabIndex = 6;
+            this.loginBtn.Text = "LOGIN";
+            this.loginBtn.UseVisualStyleBackColor = false;
+            // 
+            // passwdTxt
+            // 
+            this.passwdTxt.BackColor = System.Drawing.SystemColors.Control;
+            this.passwdTxt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.passwdTxt.Location = new System.Drawing.Point(337, 240);
+            this.passwdTxt.Multiline = true;
+            this.passwdTxt.Name = "passwdTxt";
+            this.passwdTxt.PasswordChar = '*';
+            this.passwdTxt.Size = new System.Drawing.Size(302, 29);
+            this.passwdTxt.TabIndex = 5;
+            // 
+            // UsernameTxt
+            // 
+            this.UsernameTxt.BackColor = System.Drawing.SystemColors.Control;
+            this.UsernameTxt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.UsernameTxt.Location = new System.Drawing.Point(337, 170);
+            this.UsernameTxt.Multiline = true;
+            this.UsernameTxt.Name = "UsernameTxt";
+            this.UsernameTxt.Size = new System.Drawing.Size(302, 29);
+            this.UsernameTxt.TabIndex = 4;
+            // 
+            // passwdLabel
+            // 
+            this.passwdLabel.AutoSize = true;
+            this.passwdLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwdLabel.Location = new System.Drawing.Point(336, 219);
+            this.passwdLabel.Name = "passwdLabel";
+            this.passwdLabel.Size = new System.Drawing.Size(79, 18);
+            this.passwdLabel.TabIndex = 3;
+            this.passwdLabel.Text = "Password:";
+            // 
+            // UsernameLabel
+            // 
+            this.UsernameLabel.AutoSize = true;
+            this.UsernameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UsernameLabel.Location = new System.Drawing.Point(336, 149);
+            this.UsernameLabel.Name = "UsernameLabel";
+            this.UsernameLabel.Size = new System.Drawing.Size(81, 18);
+            this.UsernameLabel.TabIndex = 2;
+            this.UsernameLabel.Text = "Username:";
+            // 
+            // WcLabel
+            // 
+            this.WcLabel.AutoSize = true;
+            this.WcLabel.Font = new System.Drawing.Font("Modern No. 20", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WcLabel.Location = new System.Drawing.Point(382, 88);
+            this.WcLabel.Name = "WcLabel";
+            this.WcLabel.Size = new System.Drawing.Size(221, 34);
+            this.WcLabel.TabIndex = 1;
+            this.WcLabel.Text = "Welcome Back!";
+            this.WcLabel.Click += new System.EventHandler(this.WcLabel_Click);
+            // 
+            // RegisterTxt
+            // 
+            this.RegisterTxt.AutoSize = true;
+            this.RegisterTxt.Font = new System.Drawing.Font("Microsoft Tai Le", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RegisterTxt.Location = new System.Drawing.Point(489, 419);
+            this.RegisterTxt.Name = "RegisterTxt";
+            this.RegisterTxt.Size = new System.Drawing.Size(90, 16);
+            this.RegisterTxt.TabIndex = 12;
+            this.RegisterTxt.Text = "Register Here";
+            this.RegisterTxt.Click += new System.EventHandler(this.RegisterTxt_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 487);
+            this.Controls.Add(this.loginPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LoginForm";
+            this.Text = "LoginForm";
+            this.loginPanel1.ResumeLayout(false);
+            this.loginPanel1.PerformLayout();
+            this.loginPanel2.ResumeLayout(false);
+            this.loginPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel loginPanel1;
+        private System.Windows.Forms.Label WcLabel;
+        private System.Windows.Forms.Label passwdLabel;
+        private System.Windows.Forms.Label UsernameLabel;
+        private System.Windows.Forms.TextBox passwdTxt;
+        private System.Windows.Forms.TextBox UsernameTxt;
+        private System.Windows.Forms.Button loginBtn;
+        private System.Windows.Forms.CheckBox showPcheck;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Panel loginPanel2;
+        private System.Windows.Forms.Label closelabel;
+        private System.Windows.Forms.Label Logolabel;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label RegisterTxt;
+    }
+}
+
---
+++ Gym Management System SDAM2/Gym Management System SDAM2/TrainerProfile.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace Gym_Management_System_SDAM2
+{
+    partial class TrainerProfile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.attendLink = new System.Windows.Forms.LinkLabel();
+            this.classLink = new System.Windows.Forms.LinkLabel();
+            this.homeLink = new System.Windows.Forms.LinkLabel();
+            this.homeLogout = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trnName = new System.Windows.Forms.Label();
+            this.trnLname = new System.Windows.Forms.Label();
+            this.trnGender = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.closelabel = new System.Windows.Forms.Label();
+            this.trnDOB = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panel1.Controls.Add(this.attendLink);
+            this.panel1.Controls.Add(this.classLink);
+            this.panel1.Controls.Add(this.homeLink);
+            this.panel1.Controls.Add(this.homeLogout);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox);
+            this.panel1.Location = new System.Drawing.Point(-1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(128, 491);
+            this.panel1.TabIndex = 0;
+            // 
+            // attendLink
+            // 
+            this.attendLink.AutoSize = true;
+            this.attendLink.BackColor = System.Drawing.Color.Transparent;
+            this.attendLink.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.attendLink.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold);
+            this.attendLink.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.attendLink.LinkColor = System.Drawing.Color.White;
+            this.attendLink.Location = new System.Drawing.Point(9, 293);
+            this.attendLink.Name = "attendLink";
+            this.attendLink.Size = new System.Drawing.Size(108, 18);
+            this.attendLink.TabIndex = 23;
+            this.attendLink.TabStop = true;
+            this.attendLink.Text = "ATTENDENCE";
+            this.attendLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.attendLink_LinkClicked);
+            // 
+            // classLink
+            // 
+            this.classLink.AutoSize = true;
+            this.classLink.BackColor = System.Drawing.Color.Transparent;
+            this.classLink.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.classLink.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold);
+            this.classLink.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.classLink.LinkColor = System.Drawing.Color.White;
+            this.classLink.Location = new System.Drawing.Point(25, 251);
+            this.classLink.Name = "classLink";
+            this.classLink.Size = new System.Drawing.Size(76, 18);
+            this.classLink.TabIndex = 22;
+            this.classLink.TabStop = true;
+            this.classLink.Text = "CLASSES";
+            this.classLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.classLink_LinkClicked);
+            // 
+            // homeLink
+            // 
+            this.homeLink.AutoSize = true;
+            this.homeLink.BackColor = System.Drawing.Color.Transparent;
+            this.homeLink.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.homeLink.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold);
+            this.homeLink.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.homeLink.LinkColor = System.Drawing.Color.White;
+            this.homeLink.Location = new System.Drawing.Point(38, 207);
+            this.homeLink.Name = "homeLink";
+            this.homeLink.Size = new System.Drawing.Size(53, 18);
+            this.homeLink.TabIndex = 21;
+            this.homeLink.TabStop = true;
+            this.homeLink.Text = "HOME";
+            this.homeLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.homeLink_LinkClicked);
+            // 
+            // homeLogout
+            // 
+            this.homeLogout.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.homeLogout.Location = new System.Drawing.Point(18, 440);
+            this.homeLogout.Name = "homeLogout";
+            this.homeLogout.Size = new System.Drawing.Size(89, 37);
+            this.homeLogout.TabIndex = 19;
+            this.homeLogout.Text = "LOG OUT";
+            this.homeLogout.UseVisualStyleBackColor = true;
+            this.homeLogout.Click += new System.EventHandler(this.homeLogout_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(25, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 17);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "TRAINER";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(13, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 17);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Logged in as: ";
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Image = global::Gym_Management_System_SDAM2.Properties.Resources.icons8_user_100;
+            this.pictureBox.Location = new System.Drawing.Point(41, 9);
+            this.pictureBox.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(40, 36);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 13;
+            this.pictureBox.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 14F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(146, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 29);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Dashboard";
+            // 
+            // trnName
+            // 
+            this.trnName.AutoSize = true;
+            this.trnName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.trnName.ForeColor = System.Drawing.Color.Black;
+            this.trnName.Location = new System.Drawing.Point(333, 79);
+            this.trnName.Name = "trnName";
+            this.trnName.Size = new System.Drawing.Size(85, 18);
+            this.trnName.TabIndex = 6;
+            this.trnName.Text = "First Name:";
+            // 
+            // trnLname
+            // 
+            this.trnLname.AutoSize = true;
+            this.trnLname.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.trnLname.ForeColor = System.Drawing.Color.Black;
+            this.trnLname.Location = new System.Drawing.Point(334, 108);
+            this.trnLname.Name = "trnLname";
+            this.trnLname.Size = new System.Drawing.Size(84, 18);
+            this.trnLname.TabIndex = 7;
+            this.trnLname.Text = "Last Name:";
+            // 
+            // trnGender
+            // 
+            this.trnGender.AutoSize = true;
+            this.trnGender.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.trnGender.ForeColor = System.Drawing.Color.Black;
+            this.trnGender.Location = new System.Drawing.Point(334, 137);
+            this.trnGender.Name = "trnGender";
+            this.trnGender.Size = new System.Drawing.Size(57, 18);
+            this.trnGender.TabIndex = 12;
+            this.trnGender.Text = "Gender";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Gym_Management_System_SDAM2.Properties.Resources.icons8_user_100;
+            this.pictureBox1.Location = new System.Drawing.Point(151, 55);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(148, 129);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // closelabel
+            // 
+            this.closelabel.AutoSize = true;
+            this.closelabel.BackColor = System.Drawing.Color.Red;
+            this.closelabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.closelabel.ForeColor = System.Drawing.Color.White;
+            this.closelabel.Location = new System.Drawing.Point(650, 9);
+            this.closelabel.Name = "closelabel";
+            this.closelabel.Size = new System.Drawing.Size(20, 20);
+            this.closelabel.TabIndex = 13;
+            this.closelabel.Text = "X";
+            // 
+            // trnDOB
+            // 
+            this.trnDOB.AutoSize = true;
+            this.trnDOB.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.trnDOB.Location = new System.Drawing.Point(333, 166);
+            this.trnDOB.Name = "trnDOB";
+            this.trnDOB.Size = new System.Drawing.Size(94, 18);
+            this.trnDOB.TabIndex = 14;
+            this.trnDOB.Text = "Date of Birth:";
+            // 
+            // TrainerProfile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 490);
+            this.ControlBox = false;
+            this.Controls.Add(this.trnDOB);
+            this.Controls.Add(this.closelabel);
+            this.Controls.Add(this.trnGender);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.trnLname);
+            this.Controls.Add(this.trnName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TrainerProfile";
+            this.Text = "TrainerProfile";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label trnName;
+        private System.Windows.Forms.Label trnLname;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label trnGender;
+        private System.Windows.Forms.Button homeLogout;
+        private System.Windows.Forms.LinkLabel homeLink;
+        private System.Windows.Forms.LinkLabel attendLink;
+        private System.Windows.Forms.LinkLabel classLink;
+        private System.Windows.Forms.Label closelabel;
+        private System.Windows.Forms.Label trnDOB;
+    }
+}
